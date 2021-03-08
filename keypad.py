8895d2fc9292175684a95e0b27b1727577ed3388
Njål Østbø <noestboe@gmail.com>
2021-03-08 17:09:48 +0100

    more keypad

--- keypad.py
+++ keypad.py
@@ -1,6 +1,9 @@
 """ Module for keypad """
+import sys
+import time
 from GPIOSimulator_v5 import *
 GPIO = GPIOSimulator()
+
 
 class Keypad:
 
@@ -23,10 +26,8 @@ class Keypad:
             (self.rows[3], self.cols[2]): '#'
         }
 
-
     def setup(self):
         """ initialize the row pins as outputs and the column pins as inputs """
-
         for rp in self.rows:
             GPIO.setup(rp, GPIO.OUT)
         for cp in self.cols:
@@ -34,12 +35,33 @@ class Keypad:
 
     def do_polling(self):
         """ Use nested loops to determine the key currently being pressed on the keypad """
+        time.sleep(0.2)
+        for rp in self.rows:
+            GPIO.output(rp, GPIO.HIGH)
+            for cp in self.cols:
+                input_sig = GPIO.input(cp)
+                if input_sig == GPIO.HIGH:
+                    return rp, cp
+            GPIO.output(rp, GPIO.LOW)
+        return None
 
     def get_next_signal(self):
         """ This is the main interface between the agent and the keypad. It should initiate
         repeated calls to do_polling until a key press is detected """
+        polling_result = self.do_polling()
+        while polling_result is None:
+            polling_result = self.do_polling()
+        return self.key_symbols[polling_result]
 
 
 if __name__ == '__main__':
     keypad = Keypad()
+    keypad.setup()
+    while True:
+        try:
+            print(keypad.get_next_signal())
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            sys.exit()
+        GPIO.cleanup()
 
